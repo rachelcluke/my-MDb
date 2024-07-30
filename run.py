a91bb2595997ab3f5bd8ceb7c7778e347f9ad52e
Rachel Luke <rachelluke716@gmail.com>
2024-07-30 09:49:03 +0000

    successful - template includes (background)

--- run.py
+++ run.py
@@ -2,21 +2,15 @@
 import os
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("pages/launch.html")
-
-@app.route("/background/")
-def background():
-    return render_template("components/background.html")
-
+    return render_template("/pages/launch.html")
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        port=int(os.environ.get("PORT", "8080")),
         debug=True) #TODO - change to debug=False before submitting project
