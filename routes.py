a3b5810d420e89feec09931b7212653e331f0f70
Rachel Luke <rachelluke716@gmail.com>
2024-08-02 13:20:06 +0000

    installed flask-migrate

--- routes.py
+++ routes.py
@@ -25,7 +25,7 @@ from flask_login import (
 from datetime import timedelta
 from werkzeug.routing import BuildError
 from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
-from app import create_app,db, login_manager,bcrypt
+from run import create_app,db, login_manager,bcrypt
 from models import MyMDB_User
 from forms import login_form, signup_form
 
@@ -50,12 +50,6 @@ def auth():
 
     return render_template("/pages/auth.html")
 
-if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "8080")),
-        debug=True) #TODO - change to debug=False before submitting project
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -68,3 +62,9 @@ def session_handler():
     app.permanent_session_lifetime = timedelta(minutes=5)
 
 #TODO - register route + logout session
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "8080")),
+        debug=True) #TODO - change to debug=False before submitting project
