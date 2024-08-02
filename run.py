16f3e99bc5e42e7b97d79966e3640df3f4ecba51
Rachel Luke <rachelluke716@gmail.com>
2024-08-02 12:43:27 +0000

    manage.py and models.py created for flask authentication

--- run.py
+++ run.py
@@ -1,9 +1,35 @@
 
 import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
+login_manager = LoginManager()
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+def create_app():
+    app = Flask(__name__)
+
+    app.secret_key = 'secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    
+    return app
 
 @app.route("/")
 def index():
@@ -18,3 +44,5 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "8080")),
         debug=True) #TODO - change to debug=False before submitting project
+
+        
