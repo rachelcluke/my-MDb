9dcc9eb13f19d07845948e30ec063f23a493cf30
Rachel Luke <rachelluke716@gmail.com>
2024-08-08 10:59:42 +0000

    requirements.txt created

--- my_mdb/models.py
+++ my_mdb/models.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 
 #user model
 class User(UserMixin, db.Model):
-    __tablename__ = "user"
+   # __tablename__ = "user"
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
