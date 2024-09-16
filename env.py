b9c82593690df49711ac524531e6175d16f33cc3
Rachel Luke <rachelluke716@gmail.com>
2024-09-16 13:17:02 +0000

    env.py added to .gitignore (and secret key changed)

--- env.py
+++ env.py
@@ -1,8 +1,8 @@
 import os
 
 os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "007")
+os.environ.setdefault("PORT", "8080")
+os.environ.setdefault("SECRET_KEY", "7even")
 os.environ.setdefault("DEBUG", "True")
 os.environ.setdefault("DEVELOPMENT", "True")
-os.environ.setdefault("DB_URL", "postgresql:///mymdb")
+os.environ.setdefault("DB_URL", "postgresql:///my_mdb")
