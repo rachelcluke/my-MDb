3e0a262482b7ec318f9a552c34a614fb2345b605
Rachel Luke <rachelluke716@gmail.com>
2024-09-17 10:19:41 +0000

    flask RegisterForm created

--- my_mdb/models.py
+++ my_mdb/models.py
@@ -4,13 +4,28 @@ from flask_wtf import wtforms
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 
-#user model
 class User(db.Model):
-   # __tablename__ = "user"
-    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     pwd = db.Column(db.String(300), nullable=False)
 
     def __repr__(self):
         return  self.id, self.username, self.pwd
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=15)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    #checking if username already exists in db
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+
+        if existing_user_username:
+            raise ValidationError(
+                'Sorry this username already exists. Please choose a different one.')
