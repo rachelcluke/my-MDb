88aa8a1b39dd58b10c2bb5483e978c3388b02654
Rachel Luke <rachelluke716@gmail.com>
2024-09-17 10:13:25 +0000

    authentication db - new imports + removed email

--- my_mdb/models.py
+++ my_mdb/models.py
@@ -1,5 +1,8 @@
 from my_mdb import db
-#from flask_login import UserMixin
+from flask_login import UserMixin
+from flask_wtf import wtforms
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 
 #user model
 class User(db.Model):
@@ -7,8 +10,7 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    pwd = db.Column(db.String(300), unique=True, nullable=False)
+    pwd = db.Column(db.String(300), nullable=False)
 
     def __repr__(self):
-        return  self.id, self.username, self.email, self.pwd
+        return  self.id, self.username, self.pwd
