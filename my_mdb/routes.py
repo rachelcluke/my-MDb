db3071e8ea82cc8a9df274c8ac004080ce03db30
Rachel Luke <rachelluke716@gmail.com>
2024-09-23 17:55:13 +0000

    pass username on main page

--- my_mdb/routes.py
+++ my_mdb/routes.py
@@ -1,4 +1,4 @@
-from flask import flash, render_template, request, redirect, url_for
+from flask import flash, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt 
@@ -14,12 +14,29 @@ def index():
 @app.route("/auth", methods=['GET', 'POST'])
 def auth():
     form = LoginForm()
-    if request.method=='POST' and form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for("main"))
+    if request.method=='POST': #and form.validate_on_submit():
+        existing_user = User.query.filter_by(username=form.username.data).first()
+
+        if existing_user:
+            print(request.form.get("username"))
+
+            if check_password_hash(
+                    existing_user[0].password, request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Hello {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "main", username=session["auth"]))
+            else:
+                # case - password mismatch
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("auth"))
+
+        else:
+            # case - username does not exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("auth"))
+
     return render_template("/pages/auth.html", title='Login', form=form)
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -48,16 +65,13 @@ def register():
 
 @app.route("/main", methods=("GET", "POST"))
 def main():
-    #if request.method == "POST":
-        #user = User(username=request.form.get("username"))
-        #db.session.add(user)
-        #db.session.commit()
-        #return redirect(url_for("auth")) 
+    if "user" in session:
+        return render_template("/pages/main.html", username=session["user"])
     return render_template("/pages/main.html", title='My Movies')
 
-@app.route('/logout', methods=['GET', 'POST'])
-@login_required
+@app.route("/logout")
 def logout():
-    logout_user()
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
     return redirect(url_for("auth"))
-    
