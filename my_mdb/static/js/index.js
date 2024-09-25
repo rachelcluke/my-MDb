b114587f4a7559437004d677a5c7ee7dcdbdff46
Rachel Luke <rachelluke716@gmail.com>
2024-09-25 16:07:50 +0000

    basic placeholder movie dialog added - TODO: add styling

--- my_mdb/static/js/index.js
+++ my_mdb/static/js/index.js
@@ -3,8 +3,13 @@
 //Query Selectors
 const movieCardImgRef = document.querySelector('.movie-card-poster');
 const dialogRef = document.querySelector("dialog");
+const closeDialogBtnRef = document.querySelector("#close-dialog-btn-div");
 
 //Event Click
 movieCardImgRef.addEventListener("click", () => {
     dialogRef.showModal();
 });
+
+closeDialogBtnRef.addEventListener("click", () => {
+    dialogRef.close();
+  });
