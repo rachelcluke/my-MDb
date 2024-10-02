975f43f03856d2ab67badb5b1f9a4599b1613c38
Rachel Luke <rachelluke716@gmail.com>
2024-10-02 13:21:44 +0000

    remove env.py from .gitignore

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "8080")
-os.environ.setdefault("SECRET_KEY", "7even")
-os.environ.setdefault("DEBUG", "True")
-os.environ.setdefault("DEVELOPMENT", "True")
-os.environ.setdefault("DB_URL", "postgresql:///my_mdb")
