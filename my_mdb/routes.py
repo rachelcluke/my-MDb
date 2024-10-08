85300fdb8211f27d6f60c218bc8058cb01c866db
Rachel Luke <rachelluke716@gmail.com>
2024-10-08 10:17:20 +0000

    my_movies() flask - multiple query RESOLVED

--- my_mdb/routes.py
+++ my_mdb/routes.py
@@ -65,10 +65,7 @@ def register():
 @app.route("/my_movies", methods=("GET", "POST"))
 def my_movies():
     if "user" in session:
-        movies = list(Movie.query.order_by(Movie.id.desc()))
-        #movies = list(Movie.query.filter(Movie.user_id==session["user_id"]). \
-                                 #Movie.query.order_by(Movie.id.desc()))
-        #movies = list(Movie.query.filter_by(Movie.user_id=session["user_id"]))
+        movies = list(Movie.query.filter(Movie.user_id==session["user_id"]).order_by(Movie.view_date.desc()))
         return render_template("/pages/main.html", username=session["user"], movies=movies)
 
     return render_template("/pages/main.html", title='My Movies', movies=movies)
